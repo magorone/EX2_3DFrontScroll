49214ded56fb7bab29b6fd6d9685609e120e4ae5
Emil <vyalshin.emil@gmail.com>
2024-08-21 16:43:46 +0300

    Первые движения

---
+++ js/app.js
@@ -0,0 +1,24 @@
+// 3D Scroll
+
+let zSpacing = -1000,
+		lastPos = zSpacing / 5,
+		$frames = document.getElementsByClassName('frame'),
+		frames = Array.from($frames),
+		zVals = []
+
+window.onscroll = function() {
+
+	let top = document.documentElement.scrollTop,
+			delta = lastPos - top
+
+	lastPos = top
+
+	frames.forEach(function(n, i) {
+		zVals.push((i * zSpacing) + zSpacing)
+		zVals[i] += delta * -5
+		let frame = frames[i],
+				transform = `translateZ(${zVals[i]}px)`
+		frame.setAttribute('style', `transform: ${transform}`)
+	})
+}
+
